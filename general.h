173ba64b1922753ab084fb17c5ae4fe2f65a5a63
Ruphane <24724395+LNSSPsd@users.noreply.github.com>
2025-05-21 16:34:17 -0700

    8217 -> 8207

--- general.h
+++ general.h
@@ -93,6 +93,7 @@ static const struct address_map_entry address_map[] = {
 
 static const struct product_id_map_entry product_id_map_preset[] = {
 	{8212, 8206, 0, 15},
+	{8217, 8207, 0, 255},
 	{8219, 8206, 0, 15}, // AirPods 4 (issue #73) -> AirPods Pro
 	{8228, 8206, 0, 15},
 	{8228, 8212, 16, 255}, // iOS 16 natively supports lightning airpods pro 2 ig
