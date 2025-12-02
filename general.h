1f19c932512155e8bc7ede6b81053c954451a5fa
r <24724395+LNSSPsd@users.noreply.github.com>
2025-12-01 18:01:21 -0800

    forgot to commit lmao

--- general.h
+++ general.h
@@ -96,8 +96,10 @@ static const struct product_id_map_entry product_id_map_preset[] = {
 	{8217, 8207, 0, 255},
 	{8219, 8206, 0, 15}, // AirPods 4 (issue #73) -> AirPods Pro
 	{8228, 8206, 0, 15},
+	{8231, 8206, 0, 15},
 	{8228, 8212, 16, 255}, // iOS 16 natively supports lightning airpods pro 2 ig
 	{8219, 8212, 16, 255}, // AirPods 4 -> AirPods Pro 2 Lightning
+	{8231, 8212, 16, 255},
 	{8211, 8207, 0, 255},
 	{8214, 8209, 0, 255},
 	{0, 0, 0, 0}
